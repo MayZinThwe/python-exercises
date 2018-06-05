58fe3e61550b557dfc494bba7b2354ddcd698238
MayZinThwe <mayzinthwe62@gmail.com>
2018-06-05 21:22:49 +0630

    My 13en python exercises

--- ex13.py
+++ ex13.py
@@ -1,6 +1,9 @@
 from sys import argv
-#read the WYSS section for how to run this
-script,first,second,third = argv
+#read the WYSS section for how to run 
+script  = argv
+first = argv
+second = argv
+third = argv
 print("The script is called:",script)
 print("Your first variable is: ",first)
 print("Your second variable is:", second)
