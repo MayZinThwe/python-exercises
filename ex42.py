abeae1f079dfecbab706c0d3bd74e03a1fcfc508
MayZinThwe <mayzinthwe62@gmail.com>
2018-06-24 19:54:11 +0630

    42 pyhton exercise

--- ex42.py
+++ ex42.py
@@ -1,61 +1,83 @@
-# Animal is-a object (yes, sort of confusing) look at the extra credit
+## Animal is-a object
 class Animal(object):
-	pass
-##??
+    pass
+
+
+## Dog is-a Animal
 class Dog(Animal):
-	def __init__(self,name):
-            ## ??
-		self.name = name 
-## ??
+
+    def __init__(self, name):
+        ## Dog has-a name
+        self.name = name
+
+
+## Cat is-a Animal
 class Cat(Animal):
 
-	def __init__(self,name):
-            ## ??
-		self.name = name 
-## ??
+    def __init__(self, name):
+        ## Cat has-a name
+        self.name = name
+
+
+## Person is-a object
 class Person(object):
 
-	def __init__(self,name):
-            ## ??
-		self.name = name 
-                ## Person has-a pet of some kind
-		self.pet = None
-## ??
+    def __init__(self, name):
+        ## Person has-a name
+        self.name = name
+
+        ## Person has-a pet of some kind
+        self.pet = None
+
+
+## Employee is-a Person
 class Employee(Person):
 
-	def __init__(self,name,salary):
-            ## ?? hmm what is this strange magic?
-		super(Employee,self).__init__(name)
-                ## ??
-		self.salary = salary
+    def __init__(self, name, salary):
+        ## ?? hmm what is this strange magic?
+        super(Employee, self).__init__(name)
+        ## Employee has-a salary
+        self.salary = salary
 
-## ??
-class Fish(object):		
-	pass
 
-## ??
+## Fish is-a object
+class Fish(object):
+    pass
+
+
+## Salmon is-a Fish
 class Salmon(Fish):
-	pass 
+    pass
 
-## ??
+
+## Halaibut is-a Fish
 class Halibut(Fish):
-	pass
+    pass
+
 
 ## rover is-a Dog
 rover = Dog("Rover")
-##??
+
+## satan is-a Cat
 satan = Cat("Satan")
-## ??
+
+## mary is-a Person
 mary = Person("Mary")
-## ??
+
+## Mary has-a pet cat name of satan
 mary.pet = satan
-## ??
-frank = Employee("Frank",120000)
-## ??
+
+## Frank is-a Employee
+frank = Employee("Frank", 120000)
+
+## Frank has-a pet dog name of rover
 frank.pet = rover
-## ??
+
+## flipper is-a Fish
 flipper = Fish()
-## ??
+
+## crouse is-a Salmon Fish
 crouse = Salmon()
-## ??
+
+## harry is-a Halibut Fish
 harry = Halibut()
