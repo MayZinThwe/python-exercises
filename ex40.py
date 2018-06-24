58f88cb4b91c3852995307fba656b652aea12b20
MayZinThwe <mayzinthwe62@gmail.com>
2018-06-24 19:06:30 +0630

    ex 40 python exercise

--- ex40.py
+++ ex40.py
@@ -1,18 +1,20 @@
 class Song(object):
-    def _init_(self, lyrics):
+
+    def __init__(self, lyrics):
         self.lyrics = lyrics
 
     def sing_me_a_song(self):
         for line in self.lyrics:
             print(line)
 
-happy_baby = Song(["Happy birthday to you",
-                   "I don't want to get sued", 
-                  "So I'll stop right there"])
 
-bulls_on_parade = Song(["They really around the famiy",
+happy_bday = Song(["Happy birthday to you",
+                   "I don't want to get sued",
+                   "So I'll stop right there"])
+
+bulls_on_parade = Song(["They rally around that family",
                         "With pockets full of shells"])
 
-happy_baby.sing_me_a_song()
+happy_bday.sing_me_a_song()
 
 bulls_on_parade.sing_me_a_song()
