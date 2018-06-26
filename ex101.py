f545ceee7ec95d0360ce0eb844d9bf34250a7f73
MayZinThwe <mayzinthwe62@gmail.com>
2018-06-26 10:12:35 +0630

    cat clone

--- ex101.py
+++ ex101.py
@@ -1,28 +1,14 @@
-tabby_cat = "\t I'm tabbed in."
-persian_cat ="I'm split \n on a line."
-backslash_cat="I'm \\ a \\ cat."
-fat_cat="""
+tabby_cat ="\tI'm tabby in."
+presian_cat = "\tI'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+fat_cat = """
 I'll do a list:
-    \t* Cat found
+    \t* cat food
     \t* Fishes
-    \t* Catnip \n \t* Grass
+    \t* Catnip\n\t* Grass
     """
 print(tabby_cat)
-print(persian_cat)
+print(presian_cat)
 print(backslash_cat)
 print(fat_cat)
-tabby_rat = "\t I'm tabbed in."
-persian_rat="I'm split \n on a line."
-backslash_rat="I'm \\ a \\ rat."
-fat_rat="""
-I'll do a list:
-    \t* Rat found
-    \t* Fishes
-    \t* Catnip \n \t* Grass
-    """
-print(tabby_rat)
-print(persian_rat)
-print(backslash_rat)
-print(fat_rat)
-
 
