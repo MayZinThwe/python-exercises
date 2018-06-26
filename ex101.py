ac2bd1c584816d3c72159c38d13265fc0fa8437f
MayZinThwe <mayzinthwe62@gmail.com>
2018-06-26 09:33:49 +0630

    Change dog

---
+++ ex101.py
@@ -0,0 +1,14 @@
+tabby_dog = "\t I'm tabbed in."
+persian_dog ="I'm split \n on a line."
+backslash_dog="I'm \\ a \\ dog."
+fat_dog="""
+I'll do a list:
+    \t* Dog found
+    \t* Fishes
+    \t* Catnip \n \t* Grass
+    """
+print(tabby_dog)
+print(persian_dog)
+print(backslash_dog)
+print(fat_dog)
+
